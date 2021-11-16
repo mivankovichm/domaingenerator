3036c76e86a84e48ed31c8d9493f6ad86d280ade
mivankovichm <nanaivank@gmail.com>
2021-11-16 01:04:34 +0000

    Generador de dominios

---
+++ app.js
@@ -0,0 +1,11 @@
+let pronart=['the','our','my', 'her']
+let adjective=['wonderful','marvelous','incredible','unbelievable']
+let noun=['journey','adventure','challenge','story']
+
+for (let i=0; i < pronart.length; i++){
+    for (let i2=0; i2 < adjective.length; i2++){
+        for (let i3=0; i3 < noun.length; i3++){
+            console.log (pronart[i], adjective [i2], noun[i3])
+        }
+    }
+}
